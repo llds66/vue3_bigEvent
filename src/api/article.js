d64a219f7fb353e5dbd9b8bff0fc8587b341f22c
lldscc <858400061@qq.com>
2024-04-21 18:10:09 +0800

    获取文章分类请求添加Pinia保存的token

--- src/api/article.js
+++ src/api/article.js
@@ -1,16 +1,14 @@
 import request from '@/utils/request.js'
-
+import { useTokenStore } from '../stores/token.js'
 /*
 * 1. 获取文章分类
 * */
 export const articleCategoryListService = () => {
-    return request({
-        url: '/category',
-        headers: {
-            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGFpbXMiOnsiaWQiOjUsInVzZXJuYW1lIjoiYWFhMDAzIn0sImV4cCI6MTcxMzY5NTI0OH0.iSTU6gXxG8ogerV9v-FYyE5Lo_ec8IWFnE8R5-CRQ-0'
-        },
-        method: 'get'
-    })
-
-  // return request.get('/category')
+    //获取token状态
+    const tokenStore = useTokenStore()
+  return request.get('/category',{
+        headers:{
+            Authorization: tokenStore.token
+        }
+  })
 }
